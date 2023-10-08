0f73e8f3a8e12b2ef258c22d80597be0ba74cac1
Lillevik <lillevik.sveen@gmail.com>
2023-10-08 20:13:39 +0200

    ryfgjrhgfyh

---
+++ GIT_OPPHAVE-2/Oppgave4.4.js
@@ -0,0 +1,45 @@
+$.ajax ({
+ url: "https://api.github.com/repos/Lillevik88/GIT_oppgave",
+ success: function(response) {
+    console.log(response)
+ }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 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 */
